276a2f25c16b27ced3bac3d6221e793bdeb9c35b
Yuri Azevedo <yuriazevedo11@gmail.com>
2020-01-17 01:47:36 -0300

    fix: GET on /devs route

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -5,7 +5,7 @@ import SearchController from './controllers/SearchController';
 
 const routes = Router();
 
-routes.get('/dev', DevController.index);
+routes.get('/devs', DevController.index);
 routes.post('/devs', DevController.store);
 
 routes.get('/search', SearchController.index);
